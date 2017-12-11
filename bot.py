dd2322dc5b8a307761e9ddf0cbde5b16560399a1
Maya Nair <msnair@emory.edu>
2017-12-11 18:31:12 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -17,7 +17,7 @@ def response_handler(request):
         user = User.select().where(User.messenger_id == messenger_parser.messenger_id).get()
     except:
         # If user doesn't exist, we create them. This would be a first time user.
-        user = User.create(messenger_id=messenger_parser.messenger_id, state='start')
+        user = User.create(messenger_id=messenger_parser.messenger_id, state='start', score=0)
         
     # Here we need to decide what we need to do next for our user
     if user.state == 'start':
@@ -50,24 +50,21 @@ def response_handler(request):
     return response()
 
 def start(messenger_parser, user):
-    send_message(user.messenger_id, 'Welcome to Blabla Kids Doll Matching Bot! To match your child with a doll, we will ask a series of questions about your childs personality. To being, please type Start')
+    send_message(user.messenger_id, 'Welcome to the Blabla Doll Sorter! We at Blabla are here to help you find the perfect doll for your child. Every doll has its own unique personality. Please answer this short quiz so that we can match you with your child’s ideal doll! To begin, please type start')
     user.state = 'question1'
     user.save()
 
 def question1(messenger_parser, user):
 
-    question_text = 'Compared to other babies, your baby is more: /n 1: Like a slow loris (quiet and calm) /n 5: Like a Chihuahua (loud and constantly active)'
+    question_text = 'Compared to other babies, your baby is more: 1: Like a slow loris (quiet and calm) 5: Like a Chihuahua (loud and constantly active)'
     
     if messenger_parser.text.lower() == 'start': 
         send_numbers_message(user.messenger_id, question_text, NUMBERS) 
         return
-    else:
-        start(messenger_parser, user)
-        return
 
     user.score += int(messenger_parser.text)
 
-    question_text = 'Does your baby generally: /n 1: Move around and play only one no one is around /n 5: Move around and play only when others are around/watching'
+    question_text = 'Does your baby generally: 1: Move around and play only one no one is around 5: Move around and play only when others are around/watching'
     send_numbers_message(user.messenger_id, question_text, NUMBERS) 
     user.state = 'question2'
     user.save()
@@ -75,7 +72,7 @@ def question1(messenger_parser, user):
 def question2(messenger_parser, user):
     user.score += int(messenger_parser.text)
 
-    question_text = 'When your infant is upset does he/she calm down by: /n 1: Being left alone or taking a nap /n 5: Receiving attention and active comforting from parents'
+    question_text = 'When your infant is upset does he/she calm down by: 1: Being left alone or taking a nap 5: Receiving attention and active comforting from parents'
     send_numbers_message(user.messenger_id, question_text, NUMBERS) 
     user.state = 'question3'
     user.save()
@@ -83,7 +80,7 @@ def question2(messenger_parser, user):
 def question3(messenger_parser, user):
     user.score += int(messenger_parser.text)
 
-    question_text = 'When engaging with other babies, is your baby: /n 1: More like Bashful Dwarf from Snow White and shy away from attention /n More like Happy Dwarf from Snow White and enjoy the company of others, actively joining them in playing'
+    question_text = 'When engaging with other babies, is your baby: 1: More like Bashful Dwarf from Snow White and shy away from attention 5: More like Happy Dwarf from Snow White and enjoy the company of others, actively joining them in playing'
     send_numbers_message(user.messenger_id, question_text, NUMBERS) 
     user.state = 'question4'
     user.save()
@@ -92,57 +89,55 @@ def question4(messenger_parser, user):
 
     user.score += int(messenger_parser.text)
 
-    question_text = 'Is your child more like: /n 1: Lisa Simpson, consistently stay with an activity for a long period of time to completion /n 5: Bart Simpson, easily distracted and start new activities frequently without finishing the previous ones'
+    question_text = 'Is your child more like: 1: Lisa Simpson, consistently stay with an activity for a long period of time to completion 5: Bart Simpson, easily distracted and start new activities frequently without finishing the previous ones'
     send_numbers_message(user.messenger_id, question_text, NUMBERS) 
-    user.state = 'question4'
+    user.state = 'question5'
     user.save() 
 
 def question5(messenger_parser, user):
     user.score += int(messenger_parser.text)
 
-    question_text = 'When interacting with strangers/new people, does your baby act more like: /n 1: Neville Longbottom and initially resist interaction and become nervous around strangers /n 5: Ron Weasley and smile and coo at new people and interactions'
+    question_text = 'When interacting with strangers/new people, does your baby act more like: 1: Neville Longbottom and initially resist interaction and become nervous around strangers 5: Ron Weasley and smile and coo at new people and interactions'
     send_numbers_message(user.messenger_id, question_text, NUMBERS) 
-    user.state = 'question5'
+    user.state = 'question6'
     user.save()
 
 def question6(messenger_parser, user):
     user.score += int(messenger_parser.text)
 
-    question_text = 'When putting your baby to sleep, is your baby: /n 1: Highly selective with where he/she sleeps (i.e. will only sleep in her cradle, with a certain stuffed animal) /n 5: Content with sleeping anywhere'
+    question_text = 'When putting your baby to sleep, is your baby: 1: Highly selective with where he/she sleeps (i.e. will only sleep in her cradle, with a certain stuffed animal) 5: Content with sleeping anywhere'
     send_numbers_message(user.messenger_id, question_text, NUMBERS) 
-    user.state = 'question6'
+    user.state = 'question7'
     user.save()  
 
 def question7(messenger_parser, user):
     user.score += int(messenger_parser.text)
 
-    question_text = 'When sharing a space with others for a long period of time, is your child more like: /n 1: Sadness from Inside Out and get overwhelmed or tired throughout the interaction /n 5: Joy from Inside Out and get energized and active throughout the interaction'
+    question_text = 'When sharing a space with others for a long period of time, is your child more like: 1: Sadness from Inside Out and get overwhelmed or tired throughout the interaction 5: Joy from Inside Out and get energized and active throughout the interaction'
     send_numbers_message(user.messenger_id, question_text, NUMBERS) 
-    user.state = 'question7'
+    user.state = 'question8'
     user.save()       
 
 def question8(messenger_parser, user):
     user.score += int(messenger_parser.text)
 
-    question_text = 'In terms of sensitivity, is your baby: /n 1: Very fussy at even the smallest provocation? (e.g. too many people, soiled diaper, etc.) /n 5: Rarely set off by inconveniences or changes to his or her environment'
+    question_text = 'In terms of sensitivity, is your baby: 1: Very fussy at even the smallest provocation? (e.g. too many people, soiled diaper, etc.) 5: Rarely set off by inconveniences or changes to his or her environment'
     send_numbers_message(user.messenger_id, question_text, NUMBERS) 
-    user.state = 'question8'
+    user.state = 'question9'
     user.save() 
 
 def question9(messenger_parser, user):
     user.score += int(messenger_parser.text)
 
-    question_text = 'Is your child: /n 1: Highly observant and notice small details in his/her surroundings /n 5: Inattentive and gloss over details, preferring to focus on the big picture instead'
+    question_text = 'Is your child: 1: Highly observant and notice small details in his/her surroundings 5: Inattentive and gloss over details, preferring to focus on the big picture instead'
     send_numbers_message(user.messenger_id, question_text, NUMBERS) 
-    user.state = 'question9'
+    user.state = 'question10'
     user.save()     
 
 def question10(messenger_parser, user):
     user.score += int(messenger_parser.text)
 
-    user.state = 'results_handler'
-    user.save()    
-
+    results_handler(messenger_parser, user)  
 
 
 # 1-10 points = quiet, 11-20 = sweet, 21-30 = lively, 31-40 = mischievous, 41-50 = wild
@@ -150,94 +145,85 @@ def question10(messenger_parser, user):
 def results_handler(messenger_parser, user):
 
     if (user.score >= 1 and user.score <= 10):
-        quiet_description = 'some text'
+        quiet_description = 'Your child’s ideal doll falls under the Quiet category, meaning that he or she is typically very reserved and calm. Dolls in the Quiet category have muted colors, calm facial expressions, and a generally very tranquil disposition. Check out these recommended dolls from the Quiet category!'
         send_message(user.messenger_id, quiet_description)
         
         elements = []
         for i in quiet_dolls:
-            title = a['name']
-            item_url = a['url']
-            image_url = a['urlImage']
+            title = i['name']
+            item_url = i['url']
+            image_url = i['urlImage']
 
             b = URLButton('Learn More', item_url)
             elements.append(GenericTemplateElement(title, item_url, image_url, '', [b])) 
 
         mess = GenericTemplateMessage(elements, user.messenger_id)
         mess.send()
-        user.state = 'start'
-        user.save()
 
     elif (user.score >= 11 and user.score <= 20):
-        sweet_description = 'some text'
+        sweet_description = 'Your child’s ideal doll falls under the Sweet category! This means that your child is generally pretty quiet and calm, but likes to socialize from time to time! Overall, dolls that fall in the Sweet category have happy-go-lucky, laid-back demeanors. Check out these recommended dolls from the Sweet category!'
         send_message(user.messenger_id, sweet_description)
         
         elements = []
         for i in sweet_dolls:
-            title = a['name']
-            item_url = a['url']
-            image_url = a['urlImage']
+            title = i['name']
+            item_url = i['url']
+            image_url = i['urlImage']
 
             b = URLButton('Learn More', item_url)
             elements.append(GenericTemplateElement(title, item_url, image_url, '', [b])) 
 
         mess = GenericTemplateMessage(elements, user.messenger_id)
         mess.send()
-        user.state = 'start'
-        user.save()
 
     elif (user.score >= 21 and user.score <= 30):
-        lively_description = 'some text'
+        lively_description = 'Your child’s ideal doll is in the Lively category! Your child possesses both introverted and extroverted tendencies, needing an equal amount of time alone as time with other people! Dolls that fall under the Lively category have some really cool outfits and bright personalities that will liven up any room! Check out these recommendations from the Lively category!'
         send_message(user.messenger_id, lively_description)
         
         elements = []
         for i in lively_dolls:
-            title = a['name']
-            item_url = a['url']
-            image_url = a['urlImage']
+            title = i['name']
+            item_url = i['url']
+            image_url = i['urlImage']
 
             b = URLButton('Learn More', item_url)
             elements.append(GenericTemplateElement(title, item_url, image_url, '', [b])) 
 
         mess = GenericTemplateMessage(elements, user.messenger_id)
         mess.send()
-        user.state = 'start'
-        user.save()
 
     elif (user.score >= 31 and user.score <= 40):
-        mischievous_description = 'some text'
+        mischievous_description = 'Your child’s ideal doll match is in the Mischievous category! This means your child is probably a bit of a trouble maker! But that’s alright, because dolls in this category have really colorful personalities, even if they are a bit rebellious as well! Check out these recommendations from the Mischievous category!'
         send_message(user.messenger_id, mischievous_description)
         
         elements = []
         for i in mischievous_dolls:
-            title = a['name']
-            item_url = a['url']
-            image_url = a['urlImage']
+            title = i['name']
+            item_url = i['url']
+            image_url = i['urlImage']
 
             b = URLButton('Learn More', item_url)
             elements.append(GenericTemplateElement(title, item_url, image_url, '', [b])) 
 
         mess = GenericTemplateMessage(elements, user.messenger_id)
         mess.send()
-        user.state = 'start'
-        user.save()
 
     elif (user.score >= 41 and user.score <= 50):
-        wild_description = 'some text'
+        wild_description = 'Your child’s ideal doll falls in the Wild category, so your child is generally a great big ball of energy! He or she loves socializing, whether it’s meeting new people or just being around others. Dolls in the Wild category are equally as spontaneous and, as the name suggests, wild! Check out these recommendations from the Wild category!'
         send_message(user.messenger_id, wild_description)
         
         elements = []
         for i in wild_dolls:
-            title = a['name']
-            item_url = a['url']
-            image_url = a['urlImage']
+            title = i['name']
+            item_url = i['url']
+            image_url = i['urlImage']
 
             b = URLButton('Learn More', item_url)
             elements.append(GenericTemplateElement(title, item_url, image_url, '', [b])) 
 
         mess = GenericTemplateMessage(elements, user.messenger_id)
         mess.send()
-        user.state = 'start'
-        user.save()
-
-    else:
-        start(messenger_parser, user)
+        
+    user.score = 0
+    user.state = 'start'
+    user.save()
